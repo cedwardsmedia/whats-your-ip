e78403e60ff3243c17cb25295134d0d6c98e6cb3
Corey Edwards <cedwardsmedia@gmail.com>
2014-01-30 23:18:05 -0600

    Updated license notice
    Signed-off-by: Corey Edwards <cedwardsmedia@gmail.com>

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<!--
-Portions Copyright (c) 1997-2014 Corey Edwards All Rights
-Reserved.
-
-This file contains Original Code and/or Modifications of Original Code
-as defined in and that are subject to the Corey Edwards Public Source
-License Version 3.0 (the 'License'). You may not use this file except
-in compliance with the License. Please obtain a copy of the License at
-http://www.cedwardsmedia.com/cepsl/ and read it before using this
-file.
-
-The Original Code and all software distributed under the License are
-distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, AND COREY EDWARDS HEREBY DISCLAIMS ALL SUCH
-WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
-NON-INFRINGEMENT. Please see the License for the specific language
-governing rights and limitations under the License.
--->
-
 <?
+/* Portions Copyright (c) 1997-2014 Corey Edwards All Rights
+ * Reserved.
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Corey Edwards Public Source
+ * License Version 3.0 (the 'License'). You may not use this file except
+ * in compliance with the License. Please obtain a copy of the License at
+ * http://www.cedwardsmedia.com/cepsl/ and read it before using this
+ * file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESSED OR IMPLIED, AND COREY EDWARDS HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+ * NON-INFRINGEMENT. Please see the License for the specific language
+ * governing rights and limitations under the License.
+*/
+
 if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])){
     $IP = $_SERVER['HTTP_CF_CONNECTING_IP']; // Get the IP address of the visitor from Cloudflare  
     } else {
@@ -32,7 +31,6 @@ if (strlen($IP) < 16) { // Check the length of the IP address to determine type
     }
 
 $agent = $_SERVER['HTTP_USER_AGENT']; // Set user agent string to $agent
-
 
 ?>
 <!DOCTYPE html>
