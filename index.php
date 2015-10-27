b1b4859dc999ffe48f5595ad1ae2399d329831d4
Corey Edwards <corey@cedwardsmedia.com>
2015-10-26 21:35:35 -0500

    Removed faulty favicon

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ if (stripos($agent, 'curl') !== false || stripos($agent, 'wget') !== false) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
 
     <title>What's your IP?</title>
 
